986e2943fb4dc19d1e5712aaa240cec8f55b6bd5
Joel Joseph <72559135+mysticjoel@users.noreply.github.com>
2025-01-07 18:04:51 +0530

    Update file 1/7/2025, 6:04:51 PM

--- 1408-StringMatchinginanArray/1408-StringMatchinginanArray.py
+++ 1408-StringMatchinginanArray/1408-StringMatchinginanArray.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         #print('abss'.find('as'))
         #s = set(words)
@@ -6,7 +6,15 @@ class Solution:
         new = []
         for i in range(len(words)):
             for j in range(len(words)):
-                if words[i]!=words[j] and words[j].find(words[i])!=-1:
+                if i!=j and words[j].find(words[i])!=-1:
                     #print(words[i],words[j])
                     new.append(words[i])
-        return list(set(new))
+        return list(set(new))'''
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        s = ' '.join(words)
+        new = []
+        for i in words:
+            if s.count(i)>1:
+                new.append(i)
+        return new
