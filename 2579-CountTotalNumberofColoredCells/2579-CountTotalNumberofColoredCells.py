c13f62864ae2a21a1118eff9f9a8880afb36966b
Joel Joseph <72559135+mysticjoel@users.noreply.github.com>
2025-03-06 03:33:13 +0530

    File updated at 3/6/2025, 3:33:12 AM

--- 2579-CountTotalNumberofColoredCells/2579-CountTotalNumberofColoredCells.py
+++ 2579-CountTotalNumberofColoredCells/2579-CountTotalNumberofColoredCells.py
@@ -1,6 +1,12 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        c=1
-        for i in range(n):
-            c+=i*4
-        return c
+        return n*n*2-2*n+1
+        
+        if n==1:return 1
+        if n==2:return 5
+        res=5
+        k=3
+        for _ in range(n-2):
+            res+=2*(k-2)*2+4
+            k+=1
+        return res
