e138a2a2d88f8b0e181c4d594723c04e8c4d41b1
Joel Joseph <72559135+mysticjoel@users.noreply.github.com>
2025-07-18 16:06:50 +0530

    Create 118-Pascal'sTriangle

---
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.java
@@ -0,0 +1,28 @@
+// Last updated: 7/18/2025, 4:06:38 PM
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    List<List<Integer>> pascal = new ArrayList<>();
+
+    public List<Integer> setValue(int i){
+        List<Integer> row = new ArrayList<>();
+        for(int j=0;j<=i;j++){
+            if(j==0||j==i){
+                row.add(1);
+            }else{
+                int val = pascal.get(i-1).get(j-1) + pascal.get(i-1).get(j);
+                row.add(val);
+            }
+        }
+        return row;
+
+    }
+    public List<List<Integer>> generate(int numRows) {
+        
+        for(int i=0;i<numRows;i++){
+            pascal.add(setValue(i));
+        }
+        return pascal;
+    }
+}
