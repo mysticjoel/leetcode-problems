cb701f007d73c26efc6e6028634b60c1c0e49325
Joel Joseph <72559135+mysticjoel@users.noreply.github.com>
2025-07-24 00:03:18 +0530

    File updated at 7/24/2025, 12:03:16 AM

--- 541-ReverseStringII/541-ReverseStringII.java
+++ 541-ReverseStringII/541-ReverseStringII.java
@@ -1,28 +1,36 @@
-// Last updated: 7/23/2025, 11:42:08 PM
+// Last updated: 7/24/2025, 12:03:16 AM
 class Solution {
-    public String reverse(String s, int i, int j) {
-        StringBuilder sb = new StringBuilder();
-        for (int idx = j; idx >= i; idx--) {
-            sb.append(s.charAt(idx));
-        }
-        return sb.toString();
-    }
-
     public String reverseStr(String s, int k) {
-        StringBuilder result = new StringBuilder();
-        int n = s.length();
+        String ans = "";
+        if (s.length() < k)
+            return new StringBuilder(s).reverse().toString();
+        else if (s.length() < 2 * k && s.length() >= k) {
+            String f = s.substring(0, k);
+            f = new StringBuilder(f).reverse().toString();
+            String se = s.substring(k, s.length());
+            return f + se;
+        } else {
 
-        for (int i = 0; i < n; i += 2 * k) {
-            int end = Math.min(i + k - 1, n - 1);
-            result.append(reverse(s, i, end));
+            int j = k;
+            for (int i = 0; i < s.length(); i += k) {
+                if (j > s.length()) {
+                    ans += new StringBuilder(s.substring(i, s.length())).reverse().toString();
+                    return ans;
+                }
+                String temp = new StringBuilder(s.substring(i, j)).reverse().toString();
+                ans += temp;
+                j += k;
+                i += k;
+                if (j > s.length()) {
+                    ans += s.substring(i, s.length());
+                    return ans;
+                }
+                ans += s.substring(i, j);
+                j += k;
 
-            int nextStart = end + 1;
-            int nextEnd = Math.min(i + 2 * k, n);
-            if (nextStart < n) {
-                result.append(s.substring(nextStart, nextEnd));
             }
-        }
 
-        return result.toString();
+        }
+        return ans;
     }
 }
