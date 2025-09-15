0cecd7121a03b7fb28cfce2abac013e9b5297957
Joel Joseph <72559135+mysticjoel@users.noreply.github.com>
2025-09-15 14:20:28 +0530

    File updated at 9/15/2025, 2:20:27 PM

--- 384-ShuffleanArray/384-ShuffleanArray.py
+++ 384-ShuffleanArray/384-ShuffleanArray.py
@@ -1,20 +1,18 @@
-# Last updated: 9/15/2025, 2:19:06 PM
+# Last updated: 9/15/2025, 2:20:27 PM
 import random
-
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.initial_nums = nums[:]
         self.nums = nums[:]
-        
+        self.copy = nums[:]
+
     def reset(self) -> List[int]:
-        self.nums[:] = self.initial_nums[:]
+        self.nums = self.copy[:]
         return self.nums
 
     def shuffle(self) -> List[int]:
         random.shuffle(self.nums)
         return self.nums
-
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
